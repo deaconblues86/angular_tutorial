c58f9c25b23b5ef14dbdaa474e1b2b5c899c7458
Brian Hammill <hammillbc@gmail.com>
2019-01-22 18:38:28 -0500

    checking everything in prior to refactor, master/detail, section of the tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+// Mocking this up which would one day come from the backend
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Marvin' },
+    { id: 12, name: 'Maven' },
+    { id: 13, name: 'Dr. Wonder' },
+    { id: 14, name: 'Rum Tum Tugger' },
+    { id: 15, name: 'Farva' },
+    { id: 16, name: 'Mac' },
+    { id: 17, name: 'Thorny' },
+    { id: 18, name: 'Rabbit' },
+    { id: 19, name: 'Booplesnoot' },
+    { id: 20, name: 'Tornado' }
+]
